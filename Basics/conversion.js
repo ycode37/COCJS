a64f313b7ca30fa11c09152b44b11d995236a0f2
ycode37 <ycode37@gmail.com>
2024-08-08 12:31:56 +0000

    #

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -17,8 +17,25 @@ let value = 7
 let negValue = -value;
 console.log(negValue);
 
+console.log('1'+2+2);
+console.log(1+2+'2'); // Left to right calculation hot hai brother
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+let x3 = 3;
+const y3 = ++x3;
+// x is 4; y is 4
+
+let x4 = 3n;
+const y4 = ++x4;
+// x2 is 4n; y2 is 4n
 
 
-
-
+console.table([x,x2,x3,x4,y,y2,y3,y4]);
 
