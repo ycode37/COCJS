b4bba16d0cda08af4e1448e88e0357e8e7313b3d
ycode37 <ycode37@gmail.com>
2024-08-08 10:39:08 +0000

    dataType

---
+++ Basics/dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //Abb naya js use hoga iss ke baad
+
+// alert('Hello') Node mai alert nahi chalta hai
+
+// Code should be Readadble
+
+// doubt aae to mdn pr search kro
+
+// ECMA standards banati hain.
+
+// bade numbr ke liye bigint ki zarrorat hoti hai not for everyday uise
+
+//null => Standalone Value
+
+// undefined = var declare ke baad value nahi dii hia but null representation hai empty value ka
+
+const myNum = null
+
+console.log(typeof(myNum)); // output dega object bc!!
+
+
---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144335;
+let accountEmail = 'hitewh@gmail.com';
+var accountPassword = '12345';
+accountCity = 'Vadodara';
+let accountState;
+
+// accountId = 3;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+
+
+
